0cb19184fa91239e061d8595ee1d401533c15af3
Douglas Parsons <dglsparsons@gmail.com>
2020-12-02 07:30:50 +0000

    Add Day1

---
+++ day1/main.py
@@ -0,0 +1,26 @@
+from copy import copy
+from functools import reduce
+
+
+def do_thing():
+    with open('input', 'r+') as f:
+        so_far = {}
+        for line in f:
+            if not line:
+                continue
+
+            val = int(line)
+            so_far[val] = [val]
+            for k, v in copy(so_far).items():
+                if len(v) > 2:
+                    continue
+
+                combined = [*v, val]
+                total = sum(combined)
+                if total == 2020:
+                    print(combined)
+                    print("total: ", reduce(lambda x, acc: x * acc, combined))
+                so_far[total] = combined
+
+
+do_thing()
